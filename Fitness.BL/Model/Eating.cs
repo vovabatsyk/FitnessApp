41ebd3b02b49bfc7e91d85e224e65e6d98b32b9e
Volodymyr Batsyk <vovabatsyk@gmail.com>
2020-10-08 00:02:37 +0300

    Created Food Model

---
+++ Fitness.BL/Model/Eating.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fitness.BL.Model
+{
+    /// <summary>
+    /// Eating Food 
+    /// </summary>
+    public class Eating
+    {
+        /// <summary>
+        /// Mealtime
+        /// </summary>
+        public DateTime Mealtime { get; }
+
+        /// <summary>
+        /// Amount of Food
+        /// </summary>
+        public Dictionary<Food, double> Foods { get; }
+
+        /// <summary>
+        /// Food User
+        /// </summary>
+        public User User { get; }
+
+        /// <summary>
+        /// Constructor Eating (User)
+        /// </summary>
+        /// <param name="user"></param>
+        public Eating(User user)
+        {
+            User = user ?? throw new ArgumentNullException(nameof(user), "Empty User");
+            Mealtime = DateTime.UtcNow;
+            Foods = new Dictionary<Food, double>();
+        }
+
+        /// <summary>
+        /// Add Food
+        /// </summary>
+        /// <param name="food">Food Name</param>
+        /// <param name="weight">Food weight</param>
+        public void Add(Food food, double weight)
+        {
+            var product = Foods.Keys.FirstOrDefault(f => f.Name.Equals(food.Name));
+
+            if (product == null)
+                Foods.Add(food, weight);
+            else
+                Foods[product] += weight;
+        }
+    }
+}
---
+++ Fitness.BL/Model/Food.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Fitness.BL.Model
+{
+    /// <summary>
+    /// Food
+    /// </summary>
+    public class Food
+    {
+        #region Methods
+
+        public string Name { get; }
+        public double Proteins { get; }
+        public double Fats { get; }
+        public double Carbohydrates { get; }
+        public double Calories { get; set; }
+        private double CaloriesOnOneGram => Calories / 100.0;
+        private double ProteinsOnOneGram => Proteins / 100.0;
+        private double FatsOnOneGram => Fats / 100.0;
+        private double CarbohydratesOnOneGram => Carbohydrates / 100.0;
+
+        #endregion
+
+        /// <summary>
+        /// Create Food with Name
+        /// </summary>
+        /// <param name="name">Food Name</param>
+        public Food(string name) : this(name, 0, 0, 0, 0) { }
+
+        /// <summary>
+        /// Create Food with Name, Proteins,Fats and Carbohydrates
+        /// </summary>
+        /// <param name="name">Food Name</param>
+        /// <param name="proteins">Proteins</param>
+        /// <param name="fats">Fats</param>
+        /// <param name="carbohydrates">Carbohydrates</param>
+        public Food(string name, double calories, double proteins, double fats, double carbohydrates)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException(nameof(name), "Empty Food Name");
+            }
+            Name = name;
+            Calories = calories / 100.0;
+            Proteins = proteins / 100.0;
+            Fats = fats / 100.0;
+            Carbohydrates = carbohydrates / 100.0;
+        }
+
+        public override string ToString() => Name;
+    }
+}
